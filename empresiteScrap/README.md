12772400e715d4ca5a19231a9bcafdb823d5ac86
David Pestana Perdomo <david.pestana.perdomo@gmail.com>
2018-10-05 14:32:17 +0200

    all

---
+++ empresiteScrap/README.md
@@ -0,0 +1,14 @@
+Este software crawlea y scrapea todo empresite
+
+npm install
+
+npm pages.js filter-string
+
+filter-string es el keyword de busqueda en empresite
+
+ejemplo:
+
+npm pages.js fabricantes-muebles
+
+
+
--- empresiteScrap/config.js
+++ empresiteScrap/config.js
@@ -1,5 +1,6 @@
 module.exports = {
     empresiteArticle: "http://localhost:3000/api/EmpresiteArticles",
     wait: 10,
-    waitEmpresite: 4000
+    waitEmpresite: 4500,
+    userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/527.162'
 }
